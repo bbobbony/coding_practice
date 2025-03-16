1d9d84ee21d09a4637cfc9b6ffb23ef8250607ba
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-03-16 23:59:22 +0900

    서기연도와 불교연도 구분하기
    2541-1998 = 543
    을 이용해 입력받은 불교연도에서 543을 빼주는 수식을 작성한다

---
+++ Change_Birth_20250316.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Change_Birth_20250316 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = a - 543;
+		
+		System.out.print(b);
+	}
+
+}
