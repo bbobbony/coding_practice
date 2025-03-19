6f417b694055b37410db1f0046329c42f145b308
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-03-19 23:32:59 +0900

    더 큰수의 덧셈
    정수형 변수는 보통 int로 많이 설정한다. 그러나 이번 문제에서는 long을 사용하였다.
    int와 long은 둘다 정수형 데이터 타입이지만 각각 4바이트 8바이트로 long은 int보다 더 큰 정수 값을 다룰 때 사용한다.
    실수형 데이터 타입인 float와 double도 마찬가지이다.
    
    <데이터 값의 범위>
       정수형: int<long
       실수형: float<double

---
+++ Number_add_20250319.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Number_add_20250319 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong(); //int말고 long으로!!
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
