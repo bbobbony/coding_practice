2016b9571ede5350c701fe6844ef97257cca6ec6
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-03-23 00:12:17 +0900

    Update Multiples_20250322.java

--- 반복문/Multiples_20250322.java
+++ 반복문/Multiples_20250322.java
@@ -8,7 +8,7 @@ public class multiples_20250322 {
 		in.close();		
 		
 		for(int i = 1; i<10;i++) {
-			System.out.println(a+" * "+i+" = "+(a*i));
+			System.out.println(a+" * "+i+" = "+(a*i)); //각 문자열 사이에 공백 반드시 넣을 것!! 안그러면 계속 오류 발생
 		}
 	}
 }
